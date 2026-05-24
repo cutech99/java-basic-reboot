bbf31e15d1aaa8876b74fa02ca6aa6fcb00a2150
cutech99 <karenhite9803@gmail.com>
2026-05-24 10:22:20 +0900

    Study Java variables

--- java-start/src/variable/ex/VarEx1.java
+++ java-start/src/variable/ex/VarEx1.java
@@ -3,11 +3,13 @@ package variable.ex;
 // 다음 코드에 반복해서 나오는 숫자 4,3 을 다른 숫자로 한번에 변경할 수 있도록
 // 변수 num1, num2를 사용하도록 변경해보세요.
 
-public class VarEx1Question {
+public class VarEx1 {
     public static void main(String[] args) {
 
-        System.out.println(4 + 3);
-        System.out.println(4 * 3);
-        System.out.println(4 - 3);
+        int num1 = 4, num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 - num2);
     }
 }
