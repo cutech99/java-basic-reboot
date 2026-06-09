979502de254e8a3f82409bd53eec5214b54caaf0
cutech99 <karenhite9803@gmail.com>
2026-06-09 14:46:11 +0900

    Solve ArrayEx

--- java-start/src/array/EnhancedFor1.java
+++ java-start/src/array/EnhancedFor1.java
@@ -15,5 +15,11 @@ public class EnhancedFor1 {
         for (int number : numbers) {
             System.out.println(number);
         }
+
+        //for-each문을 사용할 수 없는 경우, 증가하는 index 값 필요
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers" + i + "번의 결과는: " + numbers[i]);
+        }
+
     }
 }
