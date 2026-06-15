767c89d66bd24ffc8abd3b25536b2764cbdfc8ca
cutech99 <karenhite9803@gmail.com>
2026-06-16 03:06:07 +0900

    Solve MethodEx

--- java-start/src/method/ex/MethodEx1.java
+++ java-start/src/method/ex/MethodEx1.java
@@ -1,13 +1,22 @@
 package method.ex;
 
 public class MethodEx1 {
-    public static void main(String[] args) {
-        System.out.println("평균값: " + average(1,2,3));
-        System.out.println("평균값: " + average(15,25,35));
-    }
 
-    public static double average(int a, int b, int c) {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
         int sum = a + b + c;
-        return sum / 3.0;
+        double average = sum / 3.0;
+        System.out.println("평균값: " + average);
+
+        int x = 15;
+        int y = 25;
+        int z = 35;
+
+        sum = x + y + z;
+        average = sum / 3.0;
+        System.out.println("평균값: " + average);
     }
 }
--- java-start/src/method/ex/MethodEx2Ref.java
+++ java-start/src/method/ex/MethodEx2Ref.java
@@ -1,15 +1,15 @@
 package method.ex;
 
-public class MethodEx2Ref{
-    public static void main(String[] args) {
-        String message = "Hello, world!";
-        
-        printMessage(message, 3);
-        printMessage(message, 5);
-        printMessage(message, 7);
+public class MethodEx2Ref {
 
-    public static void printMessage (String message, int times) {
-        for (int i = 0; i < times; i++0) {
+    public static void main(String[] args) {
+        printMessage("Hello, world!", 3);
+        printMessage("java", 5);
+        printMessage("hi", 7);
+    }
+
+    public static void printMessage(String message, int times) {
+        for (int i = 0; i < times; i++) {
             System.out.println(message);
         }
     }
