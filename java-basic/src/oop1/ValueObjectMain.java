e2152bdcd3ba3d2345617675d51e50318547fa01
cutech99 <karenhite9803@gmail.com>
2026-06-25 01:08:38 +0900

    Study OOP

---
+++ java-basic/src/oop1/ValueObjectMain.java
@@ -0,0 +1,12 @@
+package oop1;
+
+public class ValueObjectMain {
+
+    public static void main(String[] args) {
+        ValueObject valueData = new ValueObject();
+        valueData.add();
+        valueData.add();
+        valueData.add();
+        System.out.println("최종 숫자=" + valueData.value);
+    }
+}
