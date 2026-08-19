245190483f1ed824d930123e2d3890186192a808
cutech99 <karenhite9803@gmail.com>
2026-08-20 02:00:33 +0900

    Study Immutable

--- java-mid/src/lang/object/test/RectangleMain.java
+++ java-mid/src/lang/object/test/RectangleMain.java
@@ -1,6 +1,7 @@
 package lang.object.test;
 
 public class RectangleMain {
+
     public static void main(String[] args) {
         Rectangle rect1 = new Rectangle(100, 20);
         Rectangle rect2 = new Rectangle(100, 20);
